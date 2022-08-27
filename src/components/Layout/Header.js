50736fe3091ac60bd04f95349e61348994bff915
Manish Patil <manishpatil@Manishs-MacBook-Air.local>
2022-08-27 14:58:42 -0700

    Final Changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/food.jpeg';
+import mealsImage from '../../assets/food.jpg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
